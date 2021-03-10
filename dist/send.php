684475ab3be1322ee87961587da2a3c30ef986d7
Brahyan Rodriguez <45783288+Brahyanr10@users.noreply.github.com>
2021-03-10 16:12:47 -0500

    send

--- dist/send.php
+++ dist/send.php
@@ -6,27 +6,26 @@ $mail = $_POST['email'];
 $ciudad = $_POST['ciudad'];
 
 $header = 'From: ' . $web . " \r\n";
-$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= 'X-Mailer: PHP/' . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+$header .= 'Content-Type: text/plain';
 
 $mensaje = "Nuevo registro desde led-ls.co \r\n \r\n";
-$mensaje .= "Nombre: " . $nombre . "\r\n";
-$mensaje .= "Telefono: " . $telefono . " \r\n";
-$mensaje .= "Email: " . $mail . " \r\n";
-$mensaje .= "Ciudad: " . $ciudad . " \r\n";
-$mensaje .= "Enviado el " . date('d/m/Y', time());
-
-$para = 'Info@led-ls.co';
+$mensaje .= 'Nombre: ' . $nombre . "\r\n";
+$mensaje .= 'Telefono: ' . $telefono . " \r\n";
+$mensaje .= 'Email: ' . $mail . " \r\n";
+$mensaje .= 'Ciudad: ' . $ciudad . " \r\n";
+$mensaje .= 'Enviado el ' . date('d/m/Y', time());
+// Info@led-ls.co
+$para = 'brahyan805@gmail.com';
 $asunto = 'Registro de Contacto';
-if(mail($para, $asunto, utf8_decode($mensaje), $header)) {
-echo'<script type="text/javascript">
+if (mail($para, $asunto, utf8_decode($mensaje), $header)) {
+    echo '<script type="text/javascript">
         alert("Formulario Enviado");
         window.location.href="index.html";
         </script>';
+} else {
+    echo 'Ha habido un error al enviar el formulario, inténtalo de nuevo por favor';
 }
-else {
-   echo "Ha habido un error al enviar el formulario, inténtalo de nuevo por favor";
-}
-header("Location:/");
+header('Location:/');
 ?>
